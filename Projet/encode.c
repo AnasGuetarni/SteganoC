7bd3c8505901fbf69aba6b5a8c9c21c96333b48d
KeyKey <easypl.jose@laposte.net>
2017-11-03 12:08:09 +0100

    Programme encode thread

--- Projet/encode.c
+++ Projet/encode.c
@@ -11,7 +11,6 @@
  * convert -compress none image.jpg output.ppm
  */
 
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <libgen.h>
@@ -19,18 +18,11 @@
 #include "encode_function.h"
 #include "ppm.h"
 #include <pthread.h>
-#include <errno.h>
-#include <math.h>
-
-#define NUM_THREADS 50
-#define BITS_PER_CHAR 7
 
 /**
  * Display the program's syntaxe.
  * @param argv program's command line arguments
  */
-
-int convertDecimalToBinary(int n);
 
 void usage(char **argv) {
 	fprintf(stderr, "usage: %s -- encode text_file input_image output_image thread_count where input_image and output_image are PPM files and thread_count the number of threads to use\n", basename(argv[0]));
@@ -40,126 +32,75 @@ void usage(char **argv) {
 void *thread(void *thread_id) {
         int id = *((int *) thread_id);
         printf("Hello from thread %d\n", id);
+
         return NULL;
 }
 
-int max_char_encode(img_t *img){
-	return floor((img->height * img->width) * sizeof(pixel_t) / BITS_PER_CHAR);
-}
-
-// off_t fsize(const char *filename){
-//     struct stat st;
-//
-//     if (stat(filename, &st) == 0)
-//         return st.st_size;
-//
-//     fprintf(stderr, "Cannot determine size of %s: %s\n",
-//             filename, strerror(errno));
-//
-//     return -1;
-// }
-
-unsigned long fsize(char* file)
-{
-    FILE * f = fopen(file, "r");
-    fseek(f, 0, SEEK_END);
-    unsigned long len = (unsigned long)ftell(f);
-    fclose(f);
-    return len;
-}
-
 int main(int argc, char **argv) {
-	char *input, *output;
-	char *nbT;
-	char *fileInput;
+	char *input = NULL, *output = NULL;
+	long nbT = 0;
+	char *fileInput = NULL;
 	enum PPM_TYPE type;
 	int m = 0, f = 0;
-
-	typedef struct intervalle_st {
-		int intervalMin[NUM_THREADS];
-		int intervalMax[NUM_THREADS];
-		img_t **img_out;
-	} interval_t;
-
-	// typedef struct param_st {
-	// 	int thread_id;
-	// 	char *text_cut;
-	// 	img_t **img_out;
-	// } param_t;
-
-	interval_t intervalleT;
-
-	if (argc > 5)
-	{
-		printf("Le nombre d'argument est supérieur au nombre demandé\n");
-		exit(EXIT_FAILURE);
+	
+	// Parse command line
+	if (argc == 5) {
+		//type = PPM_BINARY;
+		fileInput = argv[1];
+		input = argv[2];
+		output = argv[3];
+	} else if (argc ==  6) {
+		if (strcmp("-ascii", argv[1]) != 0) usage(argv);
+		//type = PPM_ASCII;
+		fileInput = argv[1];
+		input = argv[2];
+		output = argv[3];
+	} else {
+		usage(argv);
 	}
+
+	nbT = strtol(argv[4], NULL, 10); // Convertie la chaine de caractère qui contient le chiffre en entier
+
+	struct intervalle {
+		int intervalMin[nbT];
+		int intervalMax[nbT];
+	};
+
+	struct intervalle intervalleT;
 
 	type = PPM_BINARY;
-	fileInput = argv[1];
-	input = argv[2];
-	output = argv[3];
-	nbT = argv[4];
-
-	*nbT = *((int *) nbT);
-
-	img_t *img;
-	img = load_ppm(input);
-	int max_char = max_char_encode(img);
-	printf("max_char: %i\n", max_char);
-	int nb_char = fsize(fileInput);
-	printf("nb_char: %i\n", nb_char);
-
-	if (nb_char > max_char){
-		printf("Fichier texte trop long pour l'image\n");
-		exit(0);
-	}
 
 	long size = sizeFile(fileInput);
 
-	double value = size / NUM_THREADS;
-	printf("Size: %lu\n", size);
+	double value = size / nbT; /* problème si le nombre d'itération est un nombre a virgule ???? */
+	printf("Size: %ld\n", size);
 	printf("La valeur pour chaque thread est de: %f\n",value);
+	printf("nbT : %ld \n", nbT);
 
-	for(int i=1;i<NUM_THREADS+1;i++){
-		if(i==1)
-			intervalleT.intervalMin[i] = value * (i-1);
-		else
-			intervalleT.intervalMin[i] = value * (i-1) + 1;
-		intervalleT.intervalMax[i] = value * i;
+	for(int i=0;i<nbT;i++){
+		intervalleT.intervalMin[i] = value * i;
+		intervalleT.intervalMax[i] = value * (i+1);
 		printf("intervalMin: %i / intervalMax: %i\n",intervalleT.intervalMin[i],intervalleT.intervalMax[i]);
 	}
+	intervalleT.intervalMax[nbT-1] = size;
+	printf("intervalMax: %i\n",intervalleT.intervalMax[nbT-1]);
 
-	//printf("En dehors de la boucle\n");
-	//printf("Valeur de intervalMin: %i\n", intervalleT.intervalMin[0]);
-
-
-	pthread_t threads[NUM_THREADS];
-	// pthread_t *threads = malloc(sizeof(pthread_t) * nb_threads);//(pthread_t*)malloc(sizeof(pthread_t)); //malloc(sizeof(pthread_t) * nb_threads);
-	// param_t *threads_param = malloc(sizeof(param_t) * nb_threads);//(param_t*)malloc(sizeof(param_t));//malloc(sizeof(param_t) * nb_threads);
-
-	int tab[NUM_THREADS];
-	for (int i = 0; i < NUM_THREADS; i++) {
-	    tab[i] = i;
-	    int code = pthread_create(&threads[i], NULL, thread, &tab[i]);
-
-			if(pthread_join(threads[i], NULL) != 0) {
-		     printf("pthread_join\n");
+	pthread_t threads[nbT];
+	for (int i = 0; i < nbT; i++) {
+	    if (pthread_create(&threads[i], NULL, thread, &i) != 0) {
+            fprintf(stderr, "pthread_create failed!\n");
+            return EXIT_FAILURE;
+        }
+	    
+		if(pthread_join(threads[i], NULL) != 0) {
+		     fprintf(stderr, "pthread_join failed!\n");
 		     return EXIT_FAILURE;
-		  }
+		}
 
-	    if (code != 0) {
-        fprintf(stderr, "pthread_create failed!\n");
-        return EXIT_FAILURE;
-	    }
     }
-    pthread_exit(NULL);
-    return EXIT_SUCCESS;
-
-
-
+    
 	// Load image
-	//img_t *img = load_ppm(input);
+	img_t *img = load_ppm(input);
 	if (!img) {
 		fprintf(stderr, "Failed loading \"%s\"!\n", input); // Si l'on arrive pas a charger l'image
 		return EXIT_FAILURE; // On retourne une erreur
@@ -178,25 +119,21 @@ int main(int argc, char **argv) {
         exit(0); // On arrête immédiatement le programme
     }
 
-		// On va alors extraire les caractères du fichier
+	if ((size*2)>=(img->height*img->width)){ // si la taille du fichier est plus petite que la taille de l'image
+		printf("La taille du texte est plus grande que la capacité de l'image\n");
+		exit(0); // si c'est plus plus grand on arrete le programme
+	}
+	else
+		printf("La taille du texte est plus petite que la taille de l'image (C'est bon !)\n"); // sinon on continue le programme
+
+	// On va alors extraire les caractères du fichier
     extractionFichier(fichier);
 
-int old = 7;
-bool end = false;
-
 	// On parcours toute l'image et applique la fonction ecritureRGB
-	for (int j = 0; j < img->height && end != true; j++) { // On parcours l'image sur la hauteur
-		for (int i = 0; i < img->width && end != true; i++) { // On parcours l'image dans sa largeur
+	for (int j = 0; j < img->height; j++) { // On parcours l'image sur la hauteur
+		for (int i = 0; i < img->width; i++) { // On parcours l'image dans sa largeur
 			pixel_t *p = &img->pix[j][i]; //On place dans une structure les valeurs des pixels
-			if (m != old)
-			{
-				old = m;
-				ecritureRGB(fichier, &p->r,&p->g,&p->b,&m,&f,size); // On écrit l'encodage sur le fichier en question
-			}
-			else
-			{
-				end = true;
-			}
+			ecritureRGB(fichier, &p->r,&p->g,&p->b,&m,&f,size); // On écrit l'encodage sur le fichier en question
 		}
 	}
 
